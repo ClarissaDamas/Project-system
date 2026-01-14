fa7be6a3be95ebdb67df721100725df07c4009f8
clarissaD <physicscla@gmail.com>
2026-01-14 16:31:25 -0300

    Front Cadastro de Usuário

--- System/views.py
+++ System/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render # Importa a função render
+
+def index(request):
+    #contexto = {'nome': 'Django', 'versao': 4}
+    return render(request, 'System/index.html') 
+
+

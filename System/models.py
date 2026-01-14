87f3e110a29179786e246bf0b71fa7afe10a536c
clarissaD <physicscla@gmail.com>
2026-01-14 17:55:35 -0300

    Conectado cadastro usuario e database

--- System/models.py
+++ System/models.py
@@ -3,12 +3,17 @@
 from django.db import models
 
 class User(models.Model):
-
     Name = models.CharField(max_length=200, verbose_name='Nome completo') 
     cpf = models.CharField(max_length=11, blank=True, null=True, verbose_name='CPF')
     Telefone = models.CharField(max_length=11, blank=True, null=True, verbose_name='número de telefone')
     Email = models.EmailField(max_length=75, blank=True, null=True, verbose_name='Email') 
     DataNascimento = models.DateField(blank=True, null=True, verbose_name='Data de nascimento') 
 
+class Usuario(models.Model):
+    Name = models.CharField(max_length=200, verbose_name='Nome completo') 
+    cpf = models.CharField(max_length=11, blank=True, null=True, verbose_name='CPF')
+    Telefone = models.CharField(max_length=11, blank=True, null=True, verbose_name='número de telefone')
+    Email = models.EmailField(max_length=75, blank=True, null=True, verbose_name='Email') 
+    DataNascimento = models.DateField(blank=True, null=True, verbose_name='Data de nascimento') 
   
 
--- System/views.py
+++ System/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 
-from django.shortcuts import render # Importa a função render
+from .models import User
+
 
 def index(request):
-    #contexto = {'nome': 'Django', 'versao': 4}
     return render(request, 'System/index.html') 
 
-
+def user(request):
+    #acessar banco de dados de usuarios
+    #user = user.objects.order_by('data_added')
+    #dicionario abaixo para receber os dados ordenados
+    #context = {'user':user}
+    return render(request, 'System/caduser.html') 

e3007430de106d776257b651daaaaf092244cad8
clarissaD <physicscla@gmail.com>
2026-01-14 18:49:37 -0300

    Adicionada tabela Usuários e path Usuários

--- System/urls.py
+++ System/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path   
 from . import views
 
+#listar as funcoes na url
+
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('', views.Usuario, name = 'caduser'),
+    path('Usuários', views.List_Usuario, name = 'caduser'),
 ]
--- System/views.py
+++ System/views.py
@@ -6,9 +6,9 @@ from .models import Usuario
 def index(request):
     return render(request, 'System/index.html') 
 
-def Usuario(request):
+def List_Usuario(request):
     #acessar banco de dados de usuarios
-    #user = user.objects.order_by('data_added')
+    user = Usuario.objects.all()
     #dicionario abaixo para receber os dados ordenados
     #context = {'user':user}
-    return render(request, 'System/caduser.html') 
+    return render(request, 'System/caduser.html', {'users':user}) 

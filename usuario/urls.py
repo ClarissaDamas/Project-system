46abdb6ea3f9923976a8a6df12fda4939d4dc0ef
clarissaD <physicscla@gmail.com>
2026-01-16 15:33:00 -0300

    Funcao logout usuario

--- usuario/urls.py
+++ usuario/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     #criar novo caminho para login, diferente do padrao Django
     path('login', auth_views.LoginView.as_view(template_name='usuário/login.html'), name = 'login'),
-    #path('Users/', views.List_Usuario, name = 'Users'),
+    path('logout', views.logout_usuario, name = 'logout'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
 
+
+def logout_usuario(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 # Create your views here.

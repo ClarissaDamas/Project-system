5d4de25ca5c7e81615161e1896b41c0f1ea5c2a5
clarissaD <physicscla@gmail.com>
2026-01-16 16:54:45 -0300

    Adicionada funcao para cadastrar usuario

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     #criar novo caminho para login, diferente do padrao Django
     path('login', auth_views.LoginView.as_view(template_name='usuário/login.html'), name = 'login'),
     path('logout', views.logout_usuario, name = 'logout'),
+    path('usuário/cadastrar.html', views.cadastrar, name = 'cadastrar'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout,login,authenticate
+from .forms import cadastrarform
 
 
 def logout_usuario(request):
+    #fazer logout
     logout(request)
     return HttpResponseRedirect(reverse('index'))
-# Create your views here.
+
+def cadastrar(request):
+    #fazer cadastro usuario
+    if request.method == 'POST':
+        form = cadastrarform(dados = request.POST)
+
+        if form.is_valid():
+            novo_usuario = form.save()
+            #verificar o usuario no banco de dados
+            authenticated_user = authenticate(nome_usuario =  novo_usuario.nome_usuario, password = request.POST['password'] )
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('index'))
+        
+    else:
+        form = cadastrarform()
+
+    context = {"form": form}
+
+    return render(request, "usuário/cadastrar.html", context)
